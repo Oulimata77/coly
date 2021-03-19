4778f77635e0b824c656f10b98b140d3970cf2f0
Thomas <thomas.carbillet@gmail.com>
2021-03-19 11:50:53 +0100

    fix

--- src/views/Tab1.vue
+++ src/views/Tab1.vue
@@ -17,9 +17,9 @@
    <ion-list>
      <ion-item v-for="ocean in oceans" :key="ocean.name">
        <ion-label>
-{{ocean.name}} qui se trouve à {{ocean.lieu}} étendu de 
+{{ocean.name}} qui se trouve à {{ocean.lieu}} étendu de
  {{ocean.superficie}} Km² </ion-label>
-<ion-button class="danger" @click.prevent="delete(ocean.id)"> Delete{{ocean.name}}</ion-button>
+<ion-button class="danger" @click.prevent="deleteOcean(ocean.id)"> Delete {{ocean.name}}</ion-button>
  </ion-item>
 </ion-list>
     </ion-content>
@@ -31,47 +31,39 @@ import { db } from '../firebaseDb';
 export default  {
   components: {IonHeader, IonToolbar, IonTitle, IonContent, IonPage },
   ionViewDidEnter(){
-this.getocean();
+    this.getocean();
   },
   data(){
     return{
       oceans:[],
     }
   },
-  mounted(){
-    this.getocean();
-  },
   methods: {
     getocean() {
       db.collection("ocean").get().then((querySnapshot) => {
-    querySnapshot.forEach((doc) => {
-        // doc.data() is never undefined for query doc snapshots
-        console.log(doc.id, " => ", doc.data());
-        this.oceans.push({
-          id:doc.id,
-          name: doc.data().name,
-          superficie: doc.data().superficie,
-          lieu: doc.data().lieu
+          querySnapshot.forEach((doc) => {
+              // doc.data() is never undefined for query doc snapshots
+              console.log(doc.id, " => ", doc.data());
+              this.oceans.push({
+                id:doc.id,
+                name: doc.data().name,
+                superficie: doc.data().superficie,
+                lieu: doc.data().lieu
 
-        });
-    });
-});
+              });
+          });
+      });
     },
-  
-  
-    methods: {
-            delete(id){
-              if (window.confirm("Do you really want to delete?")) {
-                db.collection("oceans").doc(id).delete().then(() => {
-                    console.log("Document deleted!");
-                })
-                .catch((error) => {
-                    console.error(error);
-                })
-              }
-            }
-        }
+    deleteOcean(id){
+      if (window.confirm("Do you really want to delete?")) {
+        db.collection("ocean").doc(id).delete().then(() => {
+            console.log("Document deleted!");
+        })
+        .catch((error) => {
+            console.error(error);
+        })
+      }
+    }
   }
 }
-
 </script>
